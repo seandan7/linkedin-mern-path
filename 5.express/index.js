89baad47b0183ed23ac91331266f6166754ed32e
unknown <sbd12b@gmail.com>
2020-03-18 22:11:00 -0400

    add crud opp and api

--- 5.express/index.js
+++ 5.express/index.js
@@ -1,8 +1,32 @@
 import express from "express";
+import data from "./data/data.json";
 
 const app = express();
 const PORT = 3000;
 
+// this is for the public folder on path /
+app.use(express.static("public"));
+
+// this is for images folder on path images
+app.use("/images", express.static("images"));
+
 app.listen(PORT, () => {
-  console.log("TEST");
+  console.log(`Your server is running on port ${PORT}`);
+  console.log(data[0]);
+});
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.post("/newItem", (req, res) => {
+  res.send(`a post request with /newItem route on port ${PORT}`);
+});
+
+app.put("/updateItem", (req, res) => {
+  res.send(`a put request with /updateItem route on port ${PORT}`);
+});
+
+app.delete("/deleteItem", (req, res) => {
+  res.send(`a delete request with /deleteItem route on port ${PORT}`);
 });
