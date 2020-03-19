a053ea1e2369ed6ad38677a732504da23f979884
unknown <sbd12b@gmail.com>
2020-03-19 13:18:04 -0400

    completed all crud ops on mongodb api

--- 6.express-api/index.js
+++ 6.express-api/index.js
@@ -1,8 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 
 const PORT = 4000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/CRMdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//bodyParser setup
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+routes(app);
 
 app.get("/", (req, res) => {
   res.send(`Node and express server on ${PORT}`);
---
+++ 6.express-api/src/routes/crmRoutes.js
@@ -0,0 +1,27 @@
+import {
+  addNewContact,
+  getContacts,
+  getContactWithID,
+  updateContact,
+  deleteContact
+} from "../controllers/crmController";
+
+const routes = app => {
+  app
+    .route("/contact")
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from ${req.originalUrl}`);
+      console.log(`Request type ${req.method}`);
+      next();
+    }, getContacts)
+    .post(addNewContact);
+
+  app
+    .route("/contact/:contactID")
+    .get(getContactWithID)
+    .put(updateContact)
+    .delete(deleteContact);
+};
+
+export default routes;
